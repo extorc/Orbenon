c8e4e607befb88d3e833b7cb897759d15820c605
Shaurya Mittal <mittalshaurya004@gmail.com>
2022-02-13 00:00:25 +0530

    basic rendering

--- main.cpp
+++ main.cpp
@@ -21,9 +21,24 @@ int main(){
     std::cout << "Failed to initialize OpenGL context" << std::endl;
   }
 
+  float pos[] = {
+    0,0,0.5,0,0,0.5
+  };
+
+  unsigned int buffer;
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), pos, GL_STATIC_DRAW);
+
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
   while(!glfwWindowShouldClose(window)){
     glClear(GL_COLOR_BUFFER_BIT);
     glClearColor(1,0,0,1);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
     glfwPollEvents();
     glfwSwapBuffers(window);
   }
